a4f391b7f5d130a8e61ccb844d23ff7406bf6393
SoorajModi <soomodi@gmail.com>
2020-12-14 02:30:16 -0500

    Add tests for AuthorList

--- test/authorList.test.ts
+++ test/authorList.test.ts
@@ -45,3 +45,47 @@ describe("authorList testing", () => {
         ]);
     });
 });
+
+describe("authorList.note() testing", () => {
+   test("should create note citation with one author", () => {
+      expect(authorList.authorListNote()).toBe("Author One, ");
+   });
+
+   test("should create note citation with no authors", () => {
+      expect(emptyAuthorList.authorListNote()).toBe("");
+   });
+
+   test("should create note citation with two authors", () => {
+      expect(twoAuthorList.authorListNote()).toBe("Author One and Author Two, ");
+   });
+
+   test("should create note citation with three authors", () => {
+      expect(threeAuthorList.authorListNote()).toBe("Author One, Author Two and Author Three, ");
+   });
+
+   test("should create note citation with greater than three authors", () => {
+      expect(multiAuthorList.authorListNote()).toBe("Author One et al., ");
+   });
+});
+
+describe("authorList.bibliography() testing", () => {
+    test("should create bibliography citation with one author", () => {
+       expect(authorList.authorListBibliography()).toBe("One, Author. ");
+    });
+
+    test("should create bibliography citation with no authors", () => {
+       expect(emptyAuthorList.authorListBibliography()).toBe("");
+    });
+
+    test("should create bibliography citation with two authors", () => {
+       expect(twoAuthorList.authorListBibliography()).toBe("One, Author, and Author Two. ");
+    });
+
+    test("should create bibliography citation with three authors", () => {
+        expect(threeAuthorList.authorListBibliography()).toBe("One, Author, Author Two, and Author Three. ");
+    });
+
+    test("should create bibliography citation with more than authors", () => {
+       expect(multiAuthorList.authorListBibliography()).toBe("One, Author, Author Two, Author Three, and Author Four. ");
+    });
+});
